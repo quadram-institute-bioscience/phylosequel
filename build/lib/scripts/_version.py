16b025b51feb0ecca10014744ed27694cd81b58a
Sumeet Tiwari <sumeet.kumartt@gmail.com>
2023-09-06 15:48:32 +0100

    setup.cfg

--- build/lib/scripts/_version.py
+++
@@ -1,6 +0,0 @@
-# Version file
-__version__ = "0.1.0"
-
-__all__ = [
-    "__version__"
-]
--- build/lib/scripts/hellllo.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-
-
-def main():
-    import scripts.listSamples
-    scripts.listSamples.main()
